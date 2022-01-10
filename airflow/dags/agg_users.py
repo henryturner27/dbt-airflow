cdda4ac77e3219b5af1f9c75a5998baaed3bcd87
Henry Turner <henryturner27@icloud.com>
2022-01-10 11:57:01 -0800

    reorganize file structure

--- airflow/dags/agg_users.py
+++ airflow/dags/agg_users.py
@@ -8,38 +8,40 @@ dag_name = 'agg_users'
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2018, 1, 1),
     'retries': 0,
     'retry_delay': timedelta(seconds=15),
 }
+dag_args = {
+    'start_date': datetime(2021, 12, 23),
+    'schedule_interval': '@daily',
+    'catchup': False,
+    'max_active_runs': 10,
+}
 
-with DAG(dag_name, default_args=default_args, schedule_interval=None) as dag:
+with DAG(dag_name, default_args=default_args, **dag_args) as dag:
     start_task = DummyOperator(task_id='start_task')
 
     stage_task = BashOperator(
         task_id=f'dbt_run-{dag_name}_stage',
-        bash_command='''
-                source ~/dbt_venv/bin/activate &&
+        bash_command=f'''
                 cd ~/dwh &&
                 dbt run -m {dag_name}_stage --vars \'{{"ds":"{{{{ ds }}}}","ds_no_dash":"{{{{ ds_nodash }}}}"}}\'
-            '''.format(dag_name=dag_name),
+            ''',
         dag=dag)
 
     test_task = BashOperator(
         task_id=f'dbt_test-{dag_name}',
         bash_command=f'''
-            source ~/dbt_venv/bin/activate &&
             cd ~/dwh &&
             dbt test -m {dag_name}_stage --vars \'{{"ds_no_dash":"{{{{ ds_nodash }}}}"}}\'''',
         dag=dag)
 
     load_task = BashOperator(
         task_id=f'dbt_run-{dag_name}',
-        bash_command='''
-                source ~/dbt_venv/bin/activate &&
+        bash_command=f'''
                 cd ~/dwh &&
                 dbt run -m {dag_name} --vars \'{{"ds":"{{{{ ds }}}}","ds_no_dash":"{{{{ ds_nodash }}}}"}}\'
-            '''.format(dag_name=dag_name),
+            ''',
         dag=dag)
 
     end_task = DummyOperator(task_id='end_task')
--- docker/docs.dockerfile
+++ docker/docs.dockerfile
@@ -3,11 +3,10 @@ FROM python:3.9
 WORKDIR /docs
 RUN useradd -s /bin/bash -d /docs docs
 
-COPY dbt_requirements.txt .
-RUN pip install -r dbt_requirements.txt
+RUN pip install dbt-postgres==1.0.0
 
 COPY dwh/ .
 COPY profiles.yml .dbt/profiles.yml
-COPY wait_for_it.sh dwh/wait_for_it.sh
+COPY docker/wait_for_it.sh dwh/wait_for_it.sh
 
 RUN chown -R docs: /docs
